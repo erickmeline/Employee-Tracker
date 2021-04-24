9b14490c4a3ea929a7d8fc3b55822097c262ebc2
Erick Meline <erickmeline@gmail.com>
2021-04-24 10:30:33 -0700

    add menu prompts

--- server.js
+++ server.js
@@ -15,3 +15,66 @@ connection.connect((err) => {
     // do something
     // connection.end();
 });
+
+const mainMenu = () => {
+    inquirer.prompt([
+        {
+            type: 'list',
+            message: '==-- Main Menu --==',
+            name: 'option',
+            choices: [
+                'View/Modify Departments',
+                'View/Modify Roles',
+                'View/Modify Employees'
+            ]
+        }
+    ])
+}
+
+const departmentMenu = () => {
+    inquirer.prompt([
+        {
+            type: 'list',
+            message: '==-- Departments Menu --==',
+            name: 'option',
+            choices: [
+                'View All Departments',
+                'Add New Department',
+                'Modify Department',
+                'Delete Department'
+            ]
+        }
+    ])
+}
+
+const roleMenu = () => {
+    inquirer.prompt([
+        {
+            type: 'list',
+            message: '==-- Roles Menu --==',
+            name: 'option',
+            choices: [
+                'View All Roles',
+                'Add New Role',
+                'Modify Role',
+                'Delete Role'
+            ]
+        }
+    ])
+}
+
+const employeeMenu = () => {
+    inquirer.prompt([
+        {
+            type: 'list',
+            message: '==-- Employee Menu --==',
+            name: 'option',
+            choices: [
+                'View All Employees',
+                'Add New Employee',
+                'Modify Employee',
+                'Delete Employee'
+            ]
+        }
+    ])
+}
